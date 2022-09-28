3ee0c9d04edc8a28c474a961e22411d409c2c972
younghs7 <113586108+younghs7@users.noreply.github.com>
2022-09-28 23:40:44 +0900

    랜덤값을 입력받아서 입력받은 랜덤값이 소수인지 판별하는 프로그램

--- Day8/src/조별과제2.java
+++ Day8/src/조별과제2.java
@@ -1,27 +1,42 @@
 
+
 public class 조별과제2 {
 	
 	
-	//1. 소수를 판단하는 함수를 만드세요.
+	//문제) 소수를 판단하는 함수를 만드세요.
 	public static void isPrimeNumber(int parameter) 
 	{	
-//		나눴을 때 나머지가0 아니면 반복. 나눴을 때 나머지가 0이면 중단(소수가 아닌 거지) 
+
+		
 		
 		
 		boolean you소수 = true;
 		
+		
+		//입력값이 1인 경우, 2인 경우, 3이상인 경우로 나눈다.
+		
+		//1인 경우 -> 소수가 아닙니다.
 		if(parameter == 1)
 		{
 			you소수 = false;
 		}
 		
-		else
+		//2인 경우 -> 소수입니다.
+		else if(parameter == 2)
 		{
+			you소수 = true;
+		}
+		
+		//3 이상인 경우에는 %연산자로 소수 여부를 계산한다.
+		else
+		{	
+			// 입력값-2 만큼 반복한다.
 			int count = 1;
 			for(int i = 0; i < parameter-2; i++)
 			{
 				count = count + 1;
 				
+				//해당 count 숫자가 약수인지 확인한다. 약수라면 you소수에 false를 대입한다.
 				if(parameter%count == 0) 
 				{
 						you소수 = false;
@@ -31,6 +46,7 @@ public class 조별과제2 {
 		}
 		
 		
+		// you소수의 T/F 값에 따라 해당 숫자를 
 		if(you소수)
 		{
 			System.out.println("그 숫자는 소수입니다.");
@@ -55,7 +71,8 @@ public class 조별과제2 {
 				
 		System.out.println("랜덤값: "+randomNumber);
 		
-		isPrimeNumber(2);
+		//만든 소수판별함수로 랜덤값을 받아서 소수인지 출력된다.
+		isPrimeNumber(randomNumber);
 		
 		
 	
@@ -63,3 +80,4 @@ public class 조별과제2 {
 		
 	}
 }
+
