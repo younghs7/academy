1c1ff692e6000a7f9c5cd13b9d291c409374d6bc
younghs7 <113586108+younghs7@users.noreply.github.com>
2022-09-29 18:30:30 +0900

    학원 수업 백업

---
+++ 배열구조.java
@@ -0,0 +1,107 @@
+package 배열;
+
+
+
+public class 배열구조 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("더미");
+//		// 배열
+//		// 어떤 data-type를 T라고 할 때
+//		// T가 n개 연속하여 할당된 자료구자.
+//		
+//		int[] a = new int[] {11, 22, 33, 44, 55};
+//		
+//		int[] b = a;
+//		
+//		System.out.println(a[0]); //11
+//		System.out.println(b[0]); //11
+//	
+//		a[0] = 0;
+//		System.out.println(b[0]); //0
+//
+		
+		
+//		//점수 평균 구하기 이것이 자바다 예제
+//		int[] scores = new int[] {83, 90 ,87};
+//		
+//		System.out.println("scores[] :" + scores[0]);
+//		System.out.println("scores[] :" + scores[1]);
+//		System.out.println("scores[] :" + scores[2]);
+//		
+//		int sum = 0;
+//		for( int i =0; i <3; i++)
+//		{
+//			sum += scores[i];
+//		}
+//		
+//		System.out.println("총합 : "+ sum);
+//		double avg = (double) sum / 3;
+//		System.out.println("평균 : "+ avg);
+		
+		
+		
+		
+//		
+//		int [] score = null;
+//		score = new int[3];
+//		int sum=0;
+//	
+//	
+//		score[0]= 83;
+//		score[1]= 90;
+//		score[2]= 100;
+//		
+//		for(int i =0; i<3; i++)
+//		{
+//			sum = sum + score[i];
+//		
+//		}
+//		
+//		double avg = sum / 3;
+//		System.out.println("총점 = " + sum);
+//		System.out.println("평균 = " + avg);
+		
+		
+		
+//		
+//		//1. 초기값이 있는 배열. 
+//		int[] ar1 = new int[] {11, 22, 33, 44, 55};
+//		// 문법상으로는 이게 맞다.
+//		// ar2~4까지 슈거코드 / c, c++ 언어를 사용하는 사람을 흡수하기 위한 슈거코드
+//		int[] ar3 = {11, 22, 33, 44, 55};
+//		
+//		int ar2[] = new int[] {11, 22, 33, 44, 55};
+//		int ar4 [] = {11, 22, 33, 44, 55};
+//		// 잘 안 쓰는 슈거코드 ar2 ar4
+//	
+//		/*
+//		 *  int[] ar1 = new int[5]이 아닌 이유
+//		 *   자바는 프로그래머를 신뢰하지 않기 때문에, preprocess에서 5를 넣어준다. 미리 개발자가 넣을 수 없다. 
+//		 */
+//		
+//		//2. 초기값이 없는 배열.
+//		
+//		int[] ar10 = new int[5];
+//		ar2[0] = 11;
+//		ar2[1] = 22;
+//		ar2[2] = 33;
+//		ar2[3] = 44;
+//		ar2[4] = 55;
+//		
+//		
+//		int[] lotto = new int[6];
+//		
+//		getLotto(lotto);
+//		
+//		for (int i=0; i<6; i++)
+//		{
+//			System.out.println(lotto[i]);
+//		}
+//		
+//		
+		
+	}
+
+}
