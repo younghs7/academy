5887689b65c90685349c6986622a9291f433d20f
younghs7 <113586108+younghs7@users.noreply.github.com>
2022-09-30 19:03:08 +0900

    Add files via upload

---
+++ Day10MultiplicationTableMy.java
@@ -0,0 +1,42 @@
+package day10;
+
+public class Day10MultiplicationTableMy {
+
+	public static void main(String[] args) {
+		
+		
+		
+		//T2
+		//- 이중for문을 이용하여 2단부터 9단까지 출력하는 코드를 작성하시오.
+		
+		//0. 변수 설정
+		int firstDan = 2; //몇단부터 시작할지
+		int maxDan = 9; //몇단까지 실행할지
+		
+		//2. 1번 코드를 8번 반복
+		for(int j = 0; j < maxDan-1; j++) {
+			
+			//1. dan단 x1~x9까지 출력하는 코드
+			
+			System.out.println(firstDan+"단을 실행하겠습니다.");
+			
+			int count = 1; 
+			for(int i=0; i<9; i++) {
+				System.out.println(firstDan+" x "+count+" = "+ (firstDan*count) );
+				count = count + 1;
+			}
+			
+			firstDan = firstDan + 1;
+			
+		}
+		
+		
+		
+
+
+		
+		
+		System.out.println("오류 발생 더미");
+	}
+
+}
