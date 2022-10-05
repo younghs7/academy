8c5b4e7dd4156658808316614806f409cebbf286
younghs7 <113586108+younghs7@users.noreply.github.com>
2022-10-05 23:55:18 +0900

    Add files via upload

---
+++ Day12Home/Day12문제2/Day12Class_문제2MainMy.java
@@ -0,0 +1,47 @@
+package Day12문제2;
+
+
+//1차함수 출력하는 클래스를 만드세요.
+/*
+입력: 
+	1> 한점의 좌표와 기울기를 입력
+	2> 두점의 좌표를 입력
+	
+출력:
+	"y=ax+b" 형태로 출력.
+*/  
+  
+public class Day12Class_문제2MainMy {
+
+	public static void main(String[] args) {
+		
+		functionOne f = new functionOne();
+		
+		
+		
+		//좌표1 (x1, y1)
+		f.setX1(1);
+		f.setY1(2);
+		
+		//좌표2 (x2, y2)
+		f.setX2(3);
+		f.setY2(4);
+		
+		//기울기a
+		f.setA(2);
+		
+		System.out.println("기울기: "+f.getA());
+		System.out.println("좌표1: ("+f.getX1()+", "+f.getY1()+")" );
+		System.out.println("좌표2: ("+f.getX2()+", "+f.getY2()+")" );
+		f.XYA();
+
+		f.XYXY();
+		
+		
+			
+	
+		
+		
+	}
+
+}
---
+++ Day12Home/Day12수업/Day12수업Main.java
@@ -0,0 +1,46 @@
+package Day12수업;
+
+public class Day12수업Main {
+
+	public static void main(String[] args) {
+		
+		
+		
+		Student st1 = new Student(); //st1: 참조변수
+//		
+//		
+//		st1.setName("정성훈");
+//		st1.age = 40;
+//		st1.avg = 100;
+//		// 이런 식의 코드가 작동 되게끔 짜면 안 된다.
+//		
+//		
+//		System.out.println(st1.name);
+//		
+		
+		
+		st1.name = "정성훈";
+		st1.number = 1;
+		st1.kor = 80;
+		st1.eng = 70;
+		st1.math = 90;
+		
+		System.out.println(st1.name);
+		System.out.println(st1.number);
+		System.out.println(st1.kor);
+		System.out.println(st1.eng);
+		System.out.println(st1.math);
+		
+//		st1.avg = 100; (쓰기)
+//		System.out.println(st1.avg); (읽기)
+//		private이면 둘 다 불가 
+		
+		
+		
+	
+		
+		
+		
+	}
+
+}
