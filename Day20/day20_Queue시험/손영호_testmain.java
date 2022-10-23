1ca52f54585263c1b80ae0748edc6b326321a012
younghs7 <113586108+younghs7@users.noreply.github.com>
2022-10-23 22:23:16 +0900

    Queue

---
+++ Day20/day20_Queue시험/손영호_testmain.java
@@ -0,0 +1,45 @@
+package day20_Queue시험;
+
+public class 손영호_testmain {
+
+	public static void main(String[] args) {
+		
+		
+//		2.Queue 객체생성
+		손영호_queue qu = new 손영호_queue(5);
+		
+//		3.enqueue() 호출
+		
+		boolean tf;
+		tf = qu.enqueue(1);
+		tf = qu.enqueue(2);
+		tf = qu.enqueue(3);
+		tf = qu.enqueue(4);
+		tf = qu.enqueue(5);
+		
+		
+//		4.dequeue() 호출
+		tf = qu.dequeue();
+		tf = qu.dequeue();
+		tf = qu.dequeue();
+		
+		tf = qu.enqueue(6);
+		tf = qu.enqueue(7);
+		tf = qu.enqueue(8);
+		tf = qu.enqueue(9);
+		tf = qu.enqueue(10);
+		
+		tf = qu.dequeue();
+		tf = qu.dequeue();
+		
+		tf = qu.enqueue(11);
+		tf = qu.enqueue(12);
+
+//		5.printQueue() 호출
+		qu.printQueue();
+
+		
+
+	}
+
+}
