e4acafc92348bf46230be4ada26876758f8e8c1d
younghs7 <113586108+younghs7@users.noreply.github.com>
2022-11-16 18:14:19 +0900

    Add files via upload

---
+++ Day40_JS/main.js
@@ -0,0 +1,21 @@
+//  document.addEventListener('DOMContentLoaded', ()=>{내용물})
+// event : DOMContentLoaded이 발생하면 --> 내용물이 실행된다.
+
+ document.addEventListener('DOMContentLoaded', ()=>{
+    document.body.innerHTML += "<h1>Hello</h1>";
+
+    // 그 외 실행할 내용들 ...........
+
+ });  
+
+
+// ▲ 풀이 ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+ 
+// const OnContentLoaded = function()           
+// {   
+//     document.body.innerHTML += "<h1>Hello</h1>";    
+
+// }
+
+// document.addEventListener('DOMContentLoaded', OnContentLoaded);
+
